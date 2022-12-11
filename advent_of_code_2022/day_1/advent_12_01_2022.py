5958ff0e3f8c777bce8022aeee71a5eb986c3968
Thomas <tomboyo150@gmail.com>
2022-12-11 13:01:41 -0600

    Update Directory Names of Days Directory names that held the code and input for each day were excessively long and redundant. The project is already specified as the Advent of Code, so putting dvent before everything was unnecessary. The directory names are now simpler.

---
+++ advent_of_code_2022/day_1/advent_12_01_2022.py
@@ -0,0 +1,19 @@
+with open("input.txt", "r") as str_input:
+    input = str_input.read()
+
+totals = []
+top_3 = []
+elves = [cal for cal in input.split("\n\n")]
+
+for elf in elves:
+    cal_list = [int(cal) for cal in elf.split("\n") if cal.isnumeric()]
+    totals.append(sum(cal_list))
+
+print(max(totals))
+
+for i in range(3):
+    most = max(totals)
+    top_3.append(most)
+    totals.remove(most)
+
+print(sum(top_3))
