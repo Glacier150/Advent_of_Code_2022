9bf19462ceaf221db46c6c2a956a0e747fb0f8be
Thomas <tomboyo150@gmail.com>
2023-01-03 14:32:34 -0600

    Day 2 Solution Attempt + Revisions Fixed up some pathing errors caused by previous file name changes. Added comments designating which section of code is for what part of the daily problem. Completed Part 1 of Day 2, but failed to complete Part 2 as of now.

--- advent-days/day-1/solution-1.py
+++ advent-days/day-1/solution-1.py
@@ -1,15 +1,19 @@
-with open("input_1.txt", "r") as str_input: # Check that you are in the proper working directory, "day_1", so that the relative path to "input_1.txt" works as intended.
+with open("input-1.txt", "r") as str_input: # Check that you are in the proper working directory, "day_1", so that the relative path to "input_1.txt" works as intended.
     input = str_input.read()
 
 totals = []
 top_3 = []
 elves = [cal for cal in input.split("\n\n")]
 
+# Part 1
+
 for elf in elves:
     cal_list = [int(cal) for cal in elf.split("\n") if cal.isnumeric()]
     totals.append(sum(cal_list))
 
 print(max(totals))
+
+# Part 2
 
 for i in range(3):
     most = max(totals)
--- advent-days/day-2/solution-2.py
+++ advent-days/day-2/solution-2.py
@@ -1,4 +1,47 @@
-with open("input_2.txt", "r") as str_input:
+with open("input-2.txt", "r") as str_input:
     input = str_input.read()
 
-print(input)
+# Part 1
+
+score = 0
+opp_strat = ["A", "B", "C", "A"]
+your_strat = ["X", "Y", "Z", "X"]
+games = [game for game in input.split("\n")]
+
+for game in games:
+    score += game.count("X")
+    score += game.count("Y") * 2
+    score += game.count("Z") * 3
+    for i in range (3):
+        if opp_strat[i] in game and your_strat[i] in game:
+            score += 3
+        elif opp_strat[i] in game and your_strat[i+1] in game:
+            score += 6
+
+print(score)
+
+# Part 2
+
+new_score = 0
+opp_strat_2 = ["A", "B", "C"]
+shape_points = [1, 2, 3]
+
+for game in games:
+    new_score += game.count("Y") * 3
+    new_score += game.count("Z") * 6
+
+#for i in range(3):
+#    for game in games:
+#        if "X" in game:
+#            new_score += shape_points[i-1]
+#        elif "Y" in game:
+#            new_score += shape_points[i]
+#        elif "Z" in game:
+#            if i < 2:
+#                new_score += shape_points[i]
+#            elif i == 2:
+#               new_score += shape_points[0]
+
+#print(new_score)
+
+# I don't know why the above commented out code doesn't work right, I think it has something to do with the loops or the lists I use, but I don't know.
